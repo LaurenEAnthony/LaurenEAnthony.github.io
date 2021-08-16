c6a26059a87a3a2eb53434f2010b211903214356
Lea <laurenanthony247@gmail.com>
2021-08-15 22:24:40 -0400

    Redesign

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,53 @@
-var hoverElements = document.getElementsByClassName('rainbow')
+// var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+//   target: "#navbarNav",
+// });
+
+var hoverElements = document.getElementsByClassName("rainbow");
 // console.log(hoverElements)
 
-for (var i=0; i<hoverElements.length; i++ ){
-    hoverElements[i].addEventListener('mouseover', function hoverHandler(e){
-        // console.log(this.className)
-        // console.log(e.currentTarget === this)
-        currEl= e.currentTarget
-        colorPick(currEl)
-    })
+for (var i = 0; i < hoverElements.length; i++) {
+  hoverElements[i].addEventListener("mouseover", function hoverHandler(e) {
+    // console.log(this.className)
+    // console.log(e.currentTarget === this)
+    currEl = e.currentTarget;
+    colorPick(currEl);
+  });
 }
 
-function colorPick(el){
-    // var colors =[
-    //     '#E95D25',
-    //     '#692B81',
-    //     '#E51D27',
-    //     '#FFF047',
-    //     '#E51956',
-    //     '#28A3DE',
-    //     '#049848',
-    //     '#EE99C0',
-    // ]
+function colorPick(el) {
+  // var colors =[
+  //     '#E95D25',
+  //     '#692B81',
+  //     '#E51D27',
+  //     '#FFF047',
+  //     '#E51956',
+  //     '#28A3DE',
+  //     '#049848',
+  //     '#EE99C0',
+  // ]
 
-    var colors =[
-        '#FB1E6E',
-        // '#0502F1',
-        '#00ffd4',
-        '#28E10A',
-        '#FF7F00',
-        // '#F4000B',
-        '#CE02F7'
-    ]
+  // var colors = [
+  //   "#FB1E6E",
+  //   // '#0502F1',
+  //   "#00ffd4",
+  //   "#28E10A",
+  //   "#FF7F00",
+  //   // '#F4000B',
+  //   "#CE02F7",
+  // ];
 
-    var random_color = colors[Math.floor( 
-        Math.random() * colors.length)]
+  var colors = [
+    "RGB(51, 255, 0)", //green
+    "RGB(102, 0, 255)", //indigo
+    "RGB(204, 0, 255)", //purple
+    "RGB(255, 255, 0)", //yellow
+    "RGB(255, 153, 0)", //orange
+    "RGB(255, 0, 102)", //magenta
+    "RGB(0, 255, 255)", //cyan
+    "RGB(0, 51, 255)", //blue
+  ];
 
-    el.style.setProperty('--accentTest', random_color)
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+
+  el.style.setProperty("--accentTest", random_color);
 }
